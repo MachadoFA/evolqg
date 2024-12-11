d33e23b9594bd64d78b3c4d1bf6c4d1621c309fa
Fabio A Machado Dsc <macfabio@gmail.com>
2024-12-11 14:01:17 -0600

    update RemoveSize to rm isometric vector

--- R/RemoveSize.R
+++ R/RemoveSize.R
@@ -5,7 +5,7 @@
 #' @param cov.matrix Covariance matrix
 #' @details Function sets the first eigenvalue to zero.
 #' @return Altered covariance matrix with no variation on former first principal component
-#' @author Diogo Melo, Guilherme Garcia
+#' @author Diogo Melo, Guilherme Garcia, Fabio Machado
 #' @export
 #' @examples
 #' cov.matrix <- RandomMatrix(10, 1, 1, 10)
@@ -13,11 +13,19 @@
 #' eigen(cov.matrix)
 #' eigen(no.size.cov.matrix)
 #' @keywords size
-RemoveSize <- function (cov.matrix) {
-  cov.matrix.svd  <-  svd(cov.matrix)
-  size.eigen.vector <- cov.matrix.svd$u[, 1]
-  size.eigen.value <- cov.matrix.svd$d[1]
-  size.factor <- size.eigen.vector * sqrt(size.eigen.value)
+RemoveSize <- function (cov.matrix, isometric=FALSE) {
+  if (isometric) {
+    k<-nrow(cov.matrix)
+    size.vector <- rep(1/sqrt(k),times=k)
+    size.var <- Evolvability(cov.matrix,size.vector)
+    size.factor <- size.vector * sqrt(size.var)
+  } else {
+    cov.matrix.svd  <-  svd(cov.matrix)
+    size.eigen.vector <- cov.matrix.svd$u[, 1]
+    size.eigen.value <- cov.matrix.svd$d[1]
+    size.factor <- size.eigen.vector * sqrt(size.eigen.value)
+  }
+  
   cov.matrix.size.removed <- cov.matrix - size.factor %*% t(size.factor)
   return (cov.matrix.size.removed)
 }
